e84a666b77fe989d11ac38d5baeacee9883d9da6
Alexei Spirit <alexeispirit@gmail.com>
2020-05-18 21:26:34 +0500

    55-finished-auth-flow

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  GOOGLE_CLIENT_ID:
+    "280905538214-3q0au5ihp4fpr6ct6t68i3m9slk6r8ef.apps.googleusercontent.com",
+  GOOGLE_CLIENT_SECRET: "Wbj3jLBCJWfuRFmb9-qj-Ag2",
+  MONGO_URI:
+    "mongodb+srv://spirit:j0vZ1BZaMLizgwTY@cluster0-bfrfi.mongodb.net/test?retryWrites=true&w=majority",
+  COOKIE_KEY: "sdkfmlskdfslkdlkmcxvxieroje",
+};
