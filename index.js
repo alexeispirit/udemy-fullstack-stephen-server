dbcdc7f2f748ef7b569a47c44bd4f5a9fdb787d8
Alexei Spirit <alexeispirit@gmail.com>
2020-05-18 08:28:59 +0500

    46-enabling-cookies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
 const keys = require("./config/keys");
 require("./models/User");
@@ -11,6 +13,16 @@ mongoose.connect(keys.MONGO_URI, {
 });
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 60 * 60 * 24 * 1000,
+    keys: [keys.COOKIE_KEY],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
