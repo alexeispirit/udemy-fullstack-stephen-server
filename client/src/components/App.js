36a774463f30a36f04b844cd1c0174b09eef168b
Alexei Spirit <alexeispirit@gmail.com>
2020-05-19 08:48:52 +0500

    78-matching-routes-with-exact

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={Landing} />
+        <Route path="/surveys" component={Dashboard} />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
