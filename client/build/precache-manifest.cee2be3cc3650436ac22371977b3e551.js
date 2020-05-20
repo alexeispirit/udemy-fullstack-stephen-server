c2afad599c3e1703ae0b8bd6e37d63a043dae4fb
Alexei Spirit <alexeispirit@gmail.com>
2020-05-20 23:10:08 +0500

    118-routing-in-production

---
+++ client/build/precache-manifest.cee2be3cc3650436ac22371977b3e551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1bb0d47e9f8a423ba99105808c24ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bd73f8f27547e4118dd",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "4bd73f8f27547e4118dd",
+    "url": "/static/js/2.bc4127e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bc4127e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f46a7195411d94c8aa73",
+    "url": "/static/js/main.cb014f83.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
