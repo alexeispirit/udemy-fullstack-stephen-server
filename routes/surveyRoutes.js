d23b1c2ddcb1664423a65a473f4b11069f6a8c28
Alexei Spirit <alexeispirit@gmail.com>
2020-05-21 08:34:01 +0500

    133-creating-subdoc-collections

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,6 +8,15 @@ module.exports = (app) => {
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
-    const survey = new Survey({ title, body, subject });
+    const survey = new Survey({
+      title,
+      body,
+      subject,
+      recipients: recipients
+        .split(",")
+        .map((email) => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSend: Date.now(),
+    });
   });
 };
