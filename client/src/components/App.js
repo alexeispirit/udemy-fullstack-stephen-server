99f12a4c59e651602258e613814bad801eba5f84
Alexei Spirit <alexeispirit@gmail.com>
2020-05-23 22:51:50 +0500

    163-styling-form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -15,14 +15,14 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container">
-        <BrowserRouter>
+      <BrowserRouter>
+        <div className="container">
           <Header />
           <Route path="/" exact component={Landing} />
           <Route path="/surveys" exact component={Dashboard} />
           <Route path="/surveys/new" component={SurveyNew} />
-        </BrowserRouter>
-      </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
