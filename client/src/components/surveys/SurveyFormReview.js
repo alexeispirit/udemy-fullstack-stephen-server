d54e911e2777e1f1fc26b22ed6cfc55154ec9dcd
Alexei Spirit <alexeispirit@gmail.com>
2020-05-24 09:44:41 +0500

    174-finalizing-review-fields

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -4,15 +4,17 @@ import { connect } from "react-redux";
 import formFields from "./formFields";
 
 const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = formFields.map(({ label, name }) => (
+    <div key={name}>
+      <label>{label}</label>
+      <div>{formValues[name]}</div>
+    </div>
+  ));
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <div>
-        <div>
-          <label>Survey Title</label>
-          <div>{formValues.title}</div>
-        </div>
-      </div>
+      {reviewFields}
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
