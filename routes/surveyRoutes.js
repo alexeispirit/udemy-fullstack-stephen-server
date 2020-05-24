5165d8e1849bb443798eb097ed95f7ce1041156b
Alexei Spirit <alexeispirit@gmail.com>
2020-05-24 18:05:57 +0500

    193-unique-events

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,8 @@
+const { URL } = require("url");
+const _ = require("lodash");
+const { Path } = require("path-parser");
 const mongoose = require("mongoose");
+
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 const Mailer = require("../services/Mailer");
@@ -12,7 +16,22 @@ module.exports = (app) => {
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
-    console.log(req.body);
+    const events = _.map(req.body, ({ email, url }) => {
+      const pathname = new URL(url).pathname;
+      const p = new Path("/api/surveys/:surveyId/:choice");
+      const match = p.test(pathname);
+
+      if (match) {
+        return { email, ...match };
+      }
+    });
+
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqBy(compactEvents, "email", "surveyId");
+
+    console.log(uniqueEvents);
+
+    res.send({});
   });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
